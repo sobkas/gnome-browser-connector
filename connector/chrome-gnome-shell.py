fa44ccde5508cb637af2c16a573c052bead03e58
Yuri Konotopov <ykonotopov@gnome.org>
2016-11-19 22:46:12 +0300

    Version bump

--- connector/chrome-gnome-shell.py
+++ connector/chrome-gnome-shell.py
@@ -22,7 +22,7 @@ import traceback
 from select import select
 from threading import Lock
 
-CONNECTOR_VERSION	= 7.1
+CONNECTOR_VERSION	= 7.2
 DEBUG_ENABLED		= False
 
 SHELL_SCHEMA = "org.gnome.shell"
